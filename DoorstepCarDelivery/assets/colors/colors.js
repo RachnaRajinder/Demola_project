b3528e23dbee49b853d445cc43b6e5471ba5c12c
Hajax8 <t2luha01@students.oamk.fi>
2025-03-18 16:08:30 +0200

    added screens and colors.js

---
+++ DoorstepCarDelivery/assets/colors/colors.js
@@ -0,0 +1,13 @@
+//Basic colors to use in app for light and dark mode
+
+const light = {
+    colorAccent: '#FFA10A',
+    colorLightBG: '#D8D6D5',
+    colorLightBox: '#F3F3F4'
+}
+
+const dark = {
+    colorAccent: '#FFA10A',
+    colorDarkBG: '#26262A',
+    colorDarkBox: '#646568'
+}
